1788ced2d1c89b89e7a44d7d0872fe6b28b0fbf3
Mikko Lehtisalo <mikko.lehtisalo@gmail.com>
2016-01-24 12:49:03 +0000

    Initial dump

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
 # mod_gllog
-Apache module for enhancing logging of graylog-web
 
+Mod_gllog interprets the authentication cookie used by Graylog2, and injects extra information to the request context for logging purposes. This can be used to enhance the access logs for the web interface with user information.
 
+Extracted information:
+* %{gl_username}n   - The name of the authenticated user
+* %{gl_sessionid}n  - The session id
+* %{gl_signaturevalid}n - Signature check for the HMAC SHA1 signature present in the cookie
+
+Results look like the following:
+
+```
+192.168.122.1 - - [24/Jan/2016:12:38:56 +0000] "GET / HTTP/1.1" 200 3594 "-" "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0" "admin" "6cd192c1-f8e2-43c8-a061-b88e2-43c8-a061-b8" "valid"
+```
+
+# Installation and configuration
+
+Compile and install the module:
+
+```
+apxs -i -c mod_gllog.c 
+```
+
+Add openssl and your module to the httpd.conf. If you used -a with apxs, you have to just add line for loading openssl, before loading the module.
+
+```
+LoadFile /usr/lib/x86_64-linux-gnu/libssl.so
+LoadModule gllog_module /usr/lib/apache2/modules/mod_gllog.so
+```
+
+After that define a LogFormat that suits your needs.
+
+```
+LogFormat "%h %l %u %t \"%r\" %>s %O \"%{Referer}i\" \"%{User-Agent}i\" \"%{gl_username}n\" \"%{gl_sessionid}n\" \"%{gl_signaturevalid}n\"" gl_combined
+```
+
+Enable the module and logging for the VirtualHost used for web interface.
+
+```
+<VirtualHost *:80>
+		# Master switch for the module
+		GlLog On
+		# The application.secret for graylog-web, stolen from application.config
+		GlLogKey "ae9a6138c6285163f1820e68cffda6191e0717e064917ee73642c819ca8b3e39f2e60d847f2a9f585ac53479f45365cb40cbaa7d1b102b8e5181704d695dedca"
+		# Switch for checking validity of the signature
+		GlLogSignature On
+		
+		# Use the LogFormat defined earlier
+        CustomLog ${APACHE_LOG_DIR}/access.log gl_combined
+</VirtualHost>
+
+```
---
+++ mod_gllog.c
@@ -0,0 +1,266 @@
+#include "httpd.h"
+#include "http_config.h"
+#include "apr_buckets.h"
+#include "apr_general.h"
+#include "apr_lib.h"
+#include "apr_escape.h"
+#include "apr_strings.h"
+#include "util_filter.h"
+#include "http_log.h"
+#include "http_request.h"
+#include "http_core.h"
+#include "http_config.h"
+#include "ap_config.h"
+#include "ap_regex.h"
+#include <ctype.h>
+#include "openssl/bio.h"
+#include "openssl/ssl.h"
+#include "openssl/err.h"
+#include <openssl/aes.h>
+#include "http_protocol.h"
+
+static const char *gl_cookie_regex = "^PLAY_SESSION=([A-Za-z0-9_]+)-sessionid=([A-Za-z0-9_]+)$";
+static const char *gl_sessionstring_regex = "^([^\t]+)\t([a-z0-9-]+)$";
+
+module AP_MODULE_DECLARE_DATA gllog_module;
+
+typedef struct
+{
+    int bEnabled;
+    const char *sKey;
+    int bSignature;
+} GlLogConfig;
+
+typedef struct {
+	const char *signature;		/* SHA1 HMAC */
+	const char *sessionid;		/* Session token*/
+} gl_play_token;
+
+static void *GlLogCreateServerConfig(apr_pool_t *p, server_rec *s)
+{
+    GlLogConfig *pConfig = apr_pcalloc(p, sizeof *pConfig);
+
+    pConfig->bEnabled = 0;
+    pConfig->sKey = "";
+    pConfig->bSignature = 0;
+
+    return pConfig;
+}
+
+/**
+Set notes for LogFormat.
+*/
+static void set_module_notes(request_rec *r, char *username, char *sessionid, char *validity) {
+	ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_DEBUG, 0, r->server, "Setting notes username: %s sessionid: %s validity: %s", username, sessionid, validity); 
+	apr_table_set(r->notes, "gl_username", username);
+	apr_table_set(r->notes, "gl_sessionid", sessionid);
+	apr_table_set(r->notes, "gl_signaturevalid", validity);
+}
+
+/**
+Checks the HMAC SHA1 signature. Returns 1 if valid.
+*/
+static int check_signature(request_rec *r, const char *signature, const char *sessionid) {
+	GlLogConfig *pConfig = ap_get_module_config(r->server->module_config, &gllog_module);
+	
+	int result = 0;
+	char *digest = apr_pcalloc(r->pool, 64);
+	char *data = apr_psprintf(r->pool, "sessionid=%s", sessionid);
+	unsigned int i = 64;
+
+	HMAC(EVP_sha1(), pConfig->sKey, strlen(pConfig->sKey), data, strlen(data), digest, &i);
+	const char *hexdigest = apr_pescape_hex(r->pool, digest, i, 0);
+	ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_DEBUG, 0, r->server, "Generated signature %s, original %s", hexdigest, signature) ; 
+	if (apr_strnatcmp(hexdigest, signature) == 0) {
+		result = 1;
+	}
+	return result;
+}
+
+/**
+Decrypt the play-graylog session cookie's sessionid part
+*/
+static char* decrypt_sessionid(const char *sessionid, request_rec *r) {
+	GlLogConfig *pConfig = ap_get_module_config(r->server->module_config, &gllog_module);
+	
+	const char *session_binary;
+	int session_binary_len;
+	char *plaintext = NULL;
+
+	// Initialize OpenSSL
+	EVP_CIPHER_CTX *ctx;
+    ctx = EVP_CIPHER_CTX_new();
+  	EVP_CIPHER_CTX_init(ctx);
+  	EVP_CIPHER_CTX_set_padding(ctx, 1); // PKCS#5
+  	
+  	// Decode hex string to binary string
+	session_binary = apr_punescape_hex(r->pool, sessionid, 0, NULL);
+	if (session_binary == NULL) {
+		ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_NOTICE, 0, r->server, "Unable to decode hex string!"); 
+		goto cleanup;
+	}
+	ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_DEBUG, 0, r->server, "Hex decoded: %s (len=%d)", session_binary, (int) strlen(session_binary)); 
+	
+	// Use max 16 characters for key!
+	char *key;
+	if (strlen(pConfig->sKey)<=16) {
+		key = (char*) pConfig->sKey;
+	} else {
+		key = apr_pcalloc(r->pool, 17);
+		apr_cpystrn (key, pConfig->sKey, 17);
+	}
+	// AES-128 ECB... 
+	if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_128_ecb(), NULL, key, NULL)) {
+		ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_NOTICE, 0, r->server, "Error initializing decryption"); 
+		goto cleanup;
+	}
+	
+	// Reserve space for plaintext
+	session_binary_len = strlen(session_binary);
+	plaintext = apr_pcalloc(r->pool, session_binary_len+1);
+	
+	// Decrypt
+	int rounds = (session_binary_len/32)+ ((session_binary_len % 32)!=0);
+	int decrypted = 0;	// Bytes decrypted every round
+	int total = 0;		// Count the total bytes decrypted
+	int x=0;
+	while ( x < rounds ) {
+		if(1 != EVP_DecryptUpdate(ctx, plaintext+(x*32), &decrypted, session_binary+(x*32), 32)) {
+			ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_NOTICE, 0, r->server, "Error in decryption"); 
+			plaintext = NULL;
+			goto cleanup;
+		}
+		x++;
+		total += decrypted;
+	}
+	
+	plaintext[total] = 0; // End the string at correct length
+	ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_DEBUG, 0, r->server, "Decrypted: %s (len=%d)", plaintext, (int) strlen(plaintext)); 
+	
+cleanup:
+	EVP_CIPHER_CTX_free(ctx);
+	return plaintext;
+}
+
+/**
+Scan the request headers for the PLAY_SESSION cookie, and return it.
+*/
+static gl_play_token* get_session_token(request_rec *r) {
+	gl_play_token *token = NULL;
+	apr_table_entry_t *e = NULL;
+	ap_regex_t *cpat = ap_pregcomp(r->pool, gl_cookie_regex, AP_REG_EXTENDED|AP_REG_ICASE);
+	ap_regmatch_t pmatch[3];
+	
+	const apr_array_header_t *headers;
+	int i = 0;
+	
+	headers = apr_table_elts(r->headers_in);
+	e = (apr_table_entry_t*) headers->elts;
+    for(i = 0; i < headers->nelts; i++) {
+    	if (ap_regexec(cpat, e[i].val, cpat->re_nsub+1, pmatch, 0) == 0) {
+    		ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_DEBUG, 0, r->server, "Cookie sent by client: %s", e[i].val); 
+    		token = apr_palloc(r->pool, sizeof(gl_play_token));
+    		token->signature = ap_pregsub(r->pool, "$1", e[i].val, cpat->re_nsub+1, pmatch);
+    		token->sessionid = ap_pregsub(r->pool, "$2", e[i].val, cpat->re_nsub+1, pmatch);
+    		return token;
+    	}
+    }
+    return NULL;
+}
+
+/**
+Main handler for mod_gllog.
+*/
+static int gllog_handler(request_rec *r)
+{
+	GlLogConfig *pConfig = ap_get_module_config(r->server->module_config, &gllog_module);
+    if (!pConfig->bEnabled)
+        return DECLINED;
+        
+	ap_regex_t *cpat = ap_pregcomp(r->pool, gl_sessionstring_regex, AP_REG_EXTENDED|AP_REG_ICASE);
+	ap_regmatch_t pmatch[3];
+	char* sessionstring;
+	char* username = "-";
+	char* sessionid = "-";
+	char* signaturestatus = "invalid";
+	
+	gl_play_token *token = get_session_token(r);
+	if (token != NULL) {
+		ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_DEBUG, 0, r->server, "Received signature: %s sessionid: %s", token->signature, token->sessionid ); 
+		sessionstring = decrypt_sessionid(token->sessionid, r);
+		if (sessionstring != NULL) {
+			if (ap_regexec(cpat, sessionstring, cpat->re_nsub+1, pmatch, 0) == 0) {
+				username = ap_pregsub(r->pool, "$1", sessionstring, cpat->re_nsub+1, pmatch);
+				sessionid = ap_pregsub(r->pool, "$2", sessionstring, cpat->re_nsub+1, pmatch);
+    		}
+		}
+	}
+	
+	if (pConfig->bSignature) {
+		if (check_signature(r, token->signature, token->sessionid) == 1) {
+			signaturestatus = "valid";
+		}
+	} else {
+		signaturestatus = "disabled";
+	}
+	
+	set_module_notes(r, username, sessionid, signaturestatus);
+    
+    return OK;
+}
+
+static const char *GlLogEnable(cmd_parms *cmd, void *dummy, int arg)
+{
+    GlLogConfig *pConfig
+      = ap_get_module_config(cmd->server->module_config,
+                             &gllog_module);
+    pConfig->bEnabled=arg;
+
+    return NULL;
+}
+
+static const char *GlLogKey(cmd_parms *cmd, void *dummy, const char *arg)
+{
+    GlLogConfig *pConfig
+      = ap_get_module_config(cmd->server->module_config,
+                             &gllog_module);
+    pConfig->sKey=arg;
+
+    return NULL;
+}
+
+static const char *GlLogSignature(cmd_parms *cmd, void *dummy, int arg)
+{
+    GlLogConfig *pConfig
+      = ap_get_module_config(cmd->server->module_config,
+                             &gllog_module);
+    pConfig->bSignature=arg;
+
+    return NULL;
+}
+
+
+static const command_rec GlLogCmds[] =
+{
+    AP_INIT_FLAG("GlLog", GlLogEnable, NULL, RSRC_CONF, "Enable or disable mod_gllog"),
+    AP_INIT_TAKE1("GlLogKey", GlLogKey, NULL, RSRC_CONF, "Set the encryption key"),
+    AP_INIT_FLAG("GlLogSignature", GlLogSignature, NULL, RSRC_CONF, "Verify the signature"),
+    { NULL }
+};
+    
+static void GlLogRegisterHooks(apr_pool_t *p)
+{
+    ap_hook_log_transaction(gllog_handler, NULL, NULL, APR_HOOK_FIRST);
+}
+
+AP_DECLARE_MODULE(gllog) =
+{
+    STANDARD20_MODULE_STUFF,
+    NULL,
+    NULL,
+    GlLogCreateServerConfig,
+    NULL,
+    GlLogCmds,
+    GlLogRegisterHooks
+};
+
